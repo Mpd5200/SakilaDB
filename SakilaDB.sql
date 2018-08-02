1469a9bac844f084bb904d661cf60defd83d8a81
Michael Duffner <michaelduffner@Michaels-MacBook-Pro-4.local>
2018-08-02 19:00:17 -0400

    SQL

--- SakilaDB.sql
+++ SakilaDB.sql
@@ -83,7 +83,75 @@ INNER JOIN payment ON customer.customer_id = payment.customer_id
 GROUP BY payment.customer_id
 ORDER BY last_name;  
 
+SELECT title from film WHERE
+language_id IN 
+(SELECT language_id FROM language
+WHERE name = 'English')
+AND (title LIKE '%K%') OR (title LIKE '%Q%'); 
 
 
+SELECT first_name, last_name FROM actor WHERE
+actor_id IN
+(SELECT actor_id FROM film_actor
+WHERE film_id = 17); 
+
+
+SELECT customer.first_name, customer.last_name, customer.email, 
+country.country
+FROM customer
+INNER JOIN address ON 
+customer.address_id = address.address_id
+INNER JOIN city ON 
+address.city_id = city.city_id
+INNER JOIN country ON
+city.country_id = country.country_id 
+WHERE country = 'Canada'; 
+
+
+SELECT title FROM film WHERE
+film_id IN
+	(SELECT film_id FROM film_category
+		WHERE category_id IN
+          (SELECT category_id from category
+              WHERE name = 'family')); 
+              
+
+SELECT title, COUNT(rental.rental_id) AS '# Rentals'
+FROM film 
+INNER JOIN inventory ON 
+film.film_id = inventory.film_id
+INNER JOIN rental on 
+rental.inventory_id = inventory.inventory_id
+GROUP BY title
+ORDER BY COUNT('# Rentals') DESC; 
+
+
+SELECT store.store_id, SUM(payment.amount) AS 'Revenue'
+FROM store
+INNER JOIN staff ON
+store.store_id = staff.store_id
+INNER JOIN payment ON 
+payment.staff_id = staff.staff_id
+GROUP BY store_id; 
+
+
+SELECT store.store_id, city.city, country.country
+FROM store
+INNER JOIN address ON
+store.address_id = address.address_id
+INNER JOIN city ON 
+address.city_id = city.city_id
+JOIN country ON 
+city.country_id = country.country_id; 
+
+
+
+SELECT categoty.name, SUM(payment.amount) AS 'Revenue'
+FROM categoty 
+INNER JOIN staff ON
+store.store_id = staff.store_id
+INNER JOIN payment ON 
+payment.staff_id = staff.staff_id
+GROUP BY store_id; 
 
 
